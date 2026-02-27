f072011cfb44849ed3f00d6d990968cb2282a002
eritque0arcus <eritque-arcus@ikuyo.dev>
2026-02-27 00:15:55 -0600

    fix: fix build script for lua part

--- build_lua/build.sh
+++ build_lua/build.sh
@@ -8,10 +8,6 @@ BUILD_PATH="$SCRIPT_DIR/build"
 USING_CORE=$(( $(nproc) - 1 ))
 CMAKE_ARG=""
 
-if [ ! -f builtins.json ]; then
-    lua "$SCRIPT_DIR/../targets/Lua/builtins_gen.lua" builtins.json
-fi
-
 while [ "$1" != "" ]; do
     case $1 in
     -dd | --disable-debug-output)
@@ -31,11 +27,11 @@ done
 export CC=clang
 export CXX=clang++
 
-echo "[build_lua] Generating builtins.json..."
-lua "$TGT_DIR/builtins_gen.lua" "$TGT_DIR/builtins.json"
-
 echo "[build_lua] Building luaFuzzer..."
 cmake -B "$BUILD_PATH" $CMAKE_ARG "$SCRIPT_DIR"
 cmake --build "$BUILD_PATH" -j "$USING_CORE" --target luaFuzzer LuaTest LuaConvert LuaCov
+
+echo "[build_lua] Generating builtins.json..."
+lua "$TGT_DIR/builtins_gen.lua" builtins.json
 
 echo "[build_lua] Done."
